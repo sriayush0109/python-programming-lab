fcc29389be63c777d729f550251bbd016a16971d
sriayush0109 <106262793+sriayush0109@users.noreply.github.com>
2022-05-30 21:51:31 +0530

    python_program_to_find_dis._between_two_pt.

---
+++ dis._bet._two_pt..py
@@ -0,0 +1,6 @@
+x1=int(input("enter first coordinate of first pt.:"))
+y1=int(input("enter second coordinate of first pt.:"))
+x2=int(input("enter first coordinate of second pt.:"))
+y2=int(input("enter second coordinate of second pt.:"))
+dis=((x2-x1)**2 + (y2-y1)**2)**0.5
+print("Distance between two pt.:",dis)
